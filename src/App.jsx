1be614bf08b70cd62e7c6f36c4f42c106b71a147
JorgeC-Galileo <109743604+JorgeC-Galileo@users.noreply.github.com>
2024-04-22 19:03:21 -0600

    Se termino el componente TaskList

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,42 @@ return(
       <div className="d-flex align-items-center">
         <Form.Control
         type="text"
-        placeholder=""
+        placeholder="Ingresa una tarea"
+        value={taskDescription}
+       onChange={(e)=> setTaskDescription(e.target.value) }
+       className="mr-2"
         />
-      </Form.Group>
+  <Button variant="success" type="submit" className="d-flex align-items-center" >     
+  Agregar Tarea 
+    </Button>
+  </div>
+   </Form.Group>
     </Form>
 );
-
-
-
-
 };
+//TaskListComponent 
+const TaskList=({tasks,onDeleteTask})=>{
+return (
+  <>
+<h2>{tasks.length}Tareas Pendientes</h2>
+<Row xs={1} sm={2} md={2} lg={3} className="g-4">
+    {tasks.map((task,index)=>(
+      <Col key={index }>
+    <div style={{backgroundColor:'black', borderColor:'#8B4513',marginBottom:'20px',padding:'10px' }}>
+      <div style={{color:'white'}}>
+        {task.description}</div>
+        <Button variant="danger" size="sm" onClick={()=>onDeleteTask(index)}> Eliminar </Button>
+        </div>
+        </Col>
+))}  
+    
+  </Row>
+
+<Button variant="danger"  onClick={()=>onDeleteTask('all')} className="mt-4">
+  Borrar Todos
+  </Button> 
+</>
+);
+    };
+
+//
