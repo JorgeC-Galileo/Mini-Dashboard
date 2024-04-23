4e618297af63ac138777a2c53a5e0f290bafc079
JorgeC-Galileo <109743604+JorgeC-Galileo@users.noreply.github.com>
2024-04-23 14:35:39 -0600

    Finish All

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 //TaskInputForm component
 const TaskInputForm=({onAddTask}) => {
@@ -34,7 +35,7 @@ return(
 const TaskList=({tasks,onDeleteTask})=>{
 return (
   <>
-<h2>{tasks.length}Tareas Pendientes</h2>
+<h2>{tasks.length} Tareas Pendientes</h2>
 <Row xs={1} sm={2} md={2} lg={3} className="g-4">
     {tasks.map((task,index)=>(
       <Col key={index }>
@@ -55,4 +56,41 @@ return (
 );
     };
 
-//
+//App Component
+const App=()=>{
+  const[tasks,setTasks]=useState([]);
+const addTask=(description)=>{
+  setTasks([...tasks,{description}])
+};
+
+const deleteTask=(index)=>{
+  if(index==='all'){
+setTasks([]);
+  }else{
+const newTasks=[...tasks];
+newTasks.splice(index,1);
+setTasks(newTasks);
+  }
+};
+return (
+<Container className="my-5">
+  <Row className="justify-conent-center">
+    <Col xs={12} md={8}>
+      <h1 className="text-center-mb-4" style={{color:'#8B4513'}}>Mini Dashboard de Tareaas</h1>
+      <TaskInputForm onAddTask={addTask} />
+          {tasks.length > 0 ? (
+      <TaskList tasks={tasks} onDeleteTask={deleteTask} />
+      ):(
+        <div className="text-center" style={{color :'#8B4513'}}>
+        <p>!No hay tareas pendientes!</p>
+        </div>
+      )}
+      </Col>
+      </Row>
+      </Container>
+
+);
+
+};
+
+export default App;
